7dcc3474bf79f83692dbc9986b52c3ef454613f2
ashritha369 <ashrithagowda666@gmail.com>
2022-12-02 21:59:55 +0530

    deleted previous code and added, Monster Rolodex: Class Components

--- src/App.js
+++ src/App.js
@@ -1,71 +1,13 @@
+// importing component, allows us to create 'class' with the functionality of component to render things in react
 import { Component } from "react";
 import "./App.css";
-
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: "",
-    };
-    console.log(`One: 1--> Constructor`);
-  }
-
-  componentDidMount() {
-    console.log(`Three: 3--> componentDidMount`);
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      );
-  }
   render() {
-    console.log(`Two: 2--> Render`);
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
-    });
-
     return (
-      <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          onChange={(event) => {
-            console.log(`starting array is`, {
-              startingArray: this.state.monsters,
-            });
-
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(
-              () => {
-                return { searchField };
-              },
-              () => {
-                console.log(`Ending Array is`, {
-                  endingArray: this.state.monsters,
-                });
-              }
-            );
-          }}
-        />
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
-      </div>
+      <>
+        <h1>hi</h1>
+      </>
     );
   }
 }
-
 export default App;
